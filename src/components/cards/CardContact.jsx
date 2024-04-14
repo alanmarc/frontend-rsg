498bcb6e2b02edfb6ea78ea3db1de5247bc37843
alanMar <alan.marcos@smartkode.mx>
2024-04-14 03:45:06 -0600

    ultimos cambios

--- src/components/cards/CardContact.jsx
+++ src/components/cards/CardContact.jsx
@@ -1,16 +1,59 @@
+import { useState } from 'react';
 import CardContactArt from '../../styles/cardContact-art'
 
 export const CardContact = () => {
+  const regexCorreo = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleEmailChange = (event) => {
+    setEmail(event.target.value);
+  };
+
+  const handlePasswordChange = (event) => {
+    setPassword(event.target.value);
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault(); 
+    if( email == '' || password == ''){
+      alert('Debe de ingresar un correo y contraseña');
+    }
+    if(password.length < 8){
+      alert('La contraseña debe tener al menos 8 caracteres');
+    }
+
+    const isLogin = regexCorreo.test(email);
+    if(!isLogin){
+      alert('El correo ingresado no es valido');
+    }
+    if(isLogin && password.length >= 8){
+      alert("Has iniciado sesión");
+      window.location.href = "/account";
+    }
+  
+  };
+
   return (
     <CardContactArt>
         <section className='container-contact'>
           <div className='container-form'>
             <div className='form-contact'>
-              <form>
+              <form onSubmit={handleSubmit}>
                 <p>Login</p>
-                <input placeholder='Username/Correo'/>
-                <input placeholder='Password'/>
-                <button >Enviar</button>
+                <input id="email"
+                  value={email}
+                  onChange={handleEmailChange}
+                  required
+                  placeholder='Username/Correo'/>
+                <input
+                  id="password"
+                  value={password}
+                  onChange={handlePasswordChange}
+                  required
+                  type="password" 
+                  placeholder='Password'/>
+                <button type="submit">Enviar</button>
               </form>
             </div>
           </div>
